93252ac7b99c76bf680c7fba3524b7f9853a9d1e
doc <luke.czapla@frisch.org>
2020-11-26 13:40:49 -0500

    ApiContext configurations

--- src/api-context.js
+++ src/api-context.js
@@ -1,6 +1,18 @@
+import React from 'react';
+
 export const configs = {
     testing: {
-        
+        userlogin: 'http://localhost:8080/conf/user',
+        getcases: 'http://localhost:8080/getcases',
+        getpeople: 'http://localhost:8080/getstudents',
+        usecors: true
+    },
+    production: {
+        userlogin: '/conf/user',
+        getcases: '/getcases',
+        getpeople: '/getstudents',
+        usecors: false
     }
-
 }
+
+export const ApiContext = React.createContext(configs.testing);
